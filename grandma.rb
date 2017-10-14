c2224404437eb8075d0208f56f1b5d5b8d9a31d5
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-14 20:12:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif words == words.upcase
     puts "NO, NOT SINCE 1938!"
   else
